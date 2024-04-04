2cd5d6ff20beca716144d7a1ae3e294ff78602ae
trideepkumar <trideepkumar.h@gmail.com>
2024-04-04 16:52:16 +0530

    for  hosting

--- client/src/api/axiosInstance.jsx
+++ client/src/api/axiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://scrapy-v5.onrender.com/api/',
+  baseURL: 'https://scrapy-v6.onrender.com/api/',
   // baseURL: 'http://localhost:3000/api/',
 
   //timeout implementation
--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'https://scrapyy-three.vercel.app/',
+  origin: 'https://scrapyy-three.vercel.app',
   // origin: 'http://localhost:5173',
   credentials: true, 
 }));
